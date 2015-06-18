cbcecb96d850c2a974a478f4d8f2d91064f64ceb
David Gomes <davidrafagomes@gmail.com>
2015-06-18 15:27:11 +0100

    Fixes code style and renames jquery.js to app.js.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(document.body ).click(function () {
+    $('#welcomeScreen').slideUp('slow');
+  });
+
+  $('#btnMenu').click(function () {
+    $('#menu').show('slow');
+  });
+});
