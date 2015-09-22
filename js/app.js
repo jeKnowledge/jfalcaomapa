fb83c50a4c5993a1e6255043535eb3f27b733d19
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-09-22 15:20:16 +0100

    correct animations and displays

--- js/app.js
+++ js/app.js
@@ -1,29 +1,60 @@
 function appMain() {
 
 
-/*  $(document).on("tap",function(){
+
+
+$('#welcomeScreen').click(function() {
+  $('#menu').hide();
+  $('#welcomeScreen').slideUp('slow', function(){
+    $('#welcomeScreen').hide();
+  });
+  $('#mainView').show();
+  console.log("Welcome Screen swiped up")
+});
+console.log("ready!");
+  /*$(document).on("tap",function(){
     $('#welcomeScreen').slideUp('slow');
     $('#mainView').show();
 
-  }); */
+  });
 
-  $('#welcomeScreen').on("swipeup",function(){
+  //$(document.body).on("swipeup",function(){
+  //$('#welcomeScreen').on("swipeup",function(){
+  $(document.body).click(function () {
     $('#welcomeScreen').slideUp('slow');
     $('#mainView').show();
     $('#menu').hide();
   });
 
 
-
+*/
   $('#btnMenu').click(function () {
-    $('#menu').slideToggle('slow');
-  });
 
 
-  $('#menu').on("swipedown",function(){
+    if ($('#menu').is(':visible')) {
+      $('#menu').hide('slow');
+      $('#optionRow').fadeOut('fast');
+      $('#menuQuestion').fadeOut('fast');
+
+    //  $('#btnMenu').show();
+   }
+   else{
+    $('#menu').show('slow');
+    setTimeout(function(){
+      $('#optionRow').fadeIn('slow');
+      $('#menuQuestion').fadeIn('slow');
+    },150);
+
+//    $('#btnMenu').hide();
+  };
+});
+
+
+/*  $('#menu').on("swipedown",function(){
     $('#menu').slideToggle('slow');
   })
 }
-
+*/
+}
 
 $(document).ready(appMain);
